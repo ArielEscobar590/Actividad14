3ffed7df0e24a14edd653b00dc257ad5de107151
German Escobar <germanjosearielescobarescobar@gmail.com>
2025-08-08 21:10:42 -0600

    Actividad 14

---
+++ Actividad14.py
@@ -0,0 +1,42 @@
+def menu():
+    participantes = []
+    while True:
+        print("\n--- MENÚ DE OPCIONES ---")
+        print("1. Agregar participante")
+        print("2. Mostrar participantes ordenados por nombre")
+        print("3. Mostrar participantes ordenados por edad")
+        print("4. Salir")
+        opcion = int(input("Seleccione una opción: "))
+
+def agregar_participante(participantes):
+    ingreso = int(input("¿Cuantos participantes desea ingresar? : "))
+    for i in range (ingreso):
+        while True:
+            dorsal = int(input("Ingrese número de dorsal:   "))
+            if dorsal in participantes:
+                print(f"El dorsal {dorsal} ya está registrado. Intente con otro.")
+            else:
+                break
+        nombre = input("Ingrese nombre completo: ")
+        while True:
+            edad = int(input("Ingrese edad: "))
+            if edad < 0 or edad > 100:
+                print("El edad debe ser entre 0 y 100")
+            else:
+                break
+        while True:
+            categorias = {"juvenil": "Juvenil", "adulto": "Adulto", "máster": "Máster"}
+            categoria = input("Ingrese categoría (juvenil, adulto, máster): ")
+            if categoria not in categorias:
+                print("Categoría inválida. Use: juvenil, adulto o máster.")
+            else:
+                break
+        categoria = categorias[categoria]
+        participante = {
+            "dorsal": dorsal,
+            "nombre": nombre,
+            "edad": edad,
+            "categoria": categoria
+        }
+        participantes.append(participante)
+
